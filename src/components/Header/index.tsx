e4baba78e36b41d16dd99e3c08dcd24eb9178497
RafaelBertuzzi <rafael.bertuzzi@gmail.com>
2022-02-15 16:57:09 -0300

    feat: add border to active route

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link'
+import {useRouter} from "next/router";
 
 export function Header() {
+    const router = useRouter();
+
     return (
         <header className={"w-full flex flex-row justify-between items-center fixed px-10 py-5"}>
-            <img className="object-cover h-20" src="/images/logo.svg" alt="corona logo" />
+            <Link href={"/"}>
+                <a>
+                    <img className="object-cover h-20" src="/images/logo.svg" alt="corona logo" />
+                </a>
+            </Link>
             <nav>
                 <Link href={"/"}>
-                    <a className={"text-md font-bold uppercase mr-10"}>Home</a>
+                    <a className={(router.pathname == "/" ? "border-b-2 border-white" : "") + " text-md font-bold uppercase mr-10"}>Home</a>
                 </Link>
                 <Link href={"/join"}>
-                    <a className={"text-md font-bold uppercase mr-10"}>Join Us</a>
+                    <a className={(router.pathname == "/join" ? "border-b-2 border-white" : "") + " text-md font-bold uppercase mr-10"}>Join Us</a>
                 </Link>
                 <Link href={"/"}>
                     <a className={"text-md font-bold uppercase mr-10"}>About</a>
